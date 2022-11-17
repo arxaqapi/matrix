0c102cc579b44a49acad175ef379f9b28f1a11cc
arxaqapi <arxaqapi@protonmail.com>
2022-11-17 15:51:35 +0100

    feat: add stats

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ pub mod matrix_manipulation;
 pub mod matrix_math;
 pub mod matrix_linalgebra;
 pub mod matrix_access;
+pub mod matrix_statistics;
 pub mod utils;
 
+#[derive(Clone)]
 pub struct Matrix {
     pub(crate) mem: Vec<f32>,
     pub(crate) shape: (usize, usize),
---
+++ src/matrix_statistics.rs
@@ -0,0 +1,35 @@
+use crate::Matrix;
+
+impl Matrix {
+    pub fn total_mean(&self) -> f32 {
+        self.total_sum() / (self.len() as f32)
+    }
+    pub fn total_median(&self) -> f32 {
+        let mut ordered_copy = self.clone();
+        ordered_copy.mem.sort_by(| a, b | a.total_cmp(b));
+
+        let mid_ind = ordered_copy.len() / 2;
+        match ordered_copy.len() % 2 {
+            0 => (ordered_copy.mem[mid_ind - 1] + ordered_copy.mem[mid_ind]) / 2.,
+            1 => ordered_copy.mem[mid_ind],
+            _ => panic!("Impossibly strange error occured: a%2 != {{0, 1}}")
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_median_odd() {
+        let new = Matrix::from(&[1., 2., 5., 9., 735.]);
+        assert_eq!(new.total_median(), 5.);
+    }
+    #[test]
+    fn test_median_even() {
+        let new = Matrix::from(&[1., 2., 5., 9., 735., 12.]);
+        assert_eq!(new.total_median(), 7.);
+    }
+}
