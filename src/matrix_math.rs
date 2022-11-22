de8f148d76c56dd4edf73d82143f96fef2b1b9ab
arxaqapi <arxaqapi@protonmail.com>
2022-11-22 23:49:23 +0100

    feat: bunch of functions

--- src/matrix_math.rs
+++ src/matrix_math.rs
@@ -1,6 +1,58 @@
 use crate::Matrix;
 
 impl Matrix {
+    //// Helper functions
+
+    /// clip: fix the upper and lower bound of the values in the matrix
+    pub fn clip(&self, lower: f32, upper: f32) -> Self {
+        Matrix {
+            mem: self
+                .mem
+                .iter()
+                .map(|&e| {
+                    if e < lower {
+                        return lower;
+                    } else if e > upper {
+                        return upper;
+                    } else {
+                        return e;
+                    }
+                })
+                .collect(),
+            ..*self
+        }
+    }
+    /// Returns the sign of the matrix
+    /// TODO: use total_cm or other comparision method for float equality
+    pub fn sign(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|&e|
+                // e.par
+                if e == 0. {
+                    return 0.;
+                } else if e < 0. {
+                    return -1.;
+                } else if e > 0.{
+                    return 1.;
+                } else {
+                    panic!("float comparison error")
+                }
+            ).collect(),
+            ..*self
+        }
+    }
+    /// absolute: calculate the absolute value element wise of the input matrix
+    pub fn absolute(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.abs()).collect(),
+            ..*self
+        }
+    }
+    /// shorthand for absolute
+    pub fn abs(&self) -> Self {
+        self.absolute()
+    }
+
     //// Operators
     /// exp: natural exponential, inverso of nat log
     pub fn exp(&self) -> Self {
@@ -15,6 +67,10 @@ impl Matrix {
             mem: self.mem.iter().map(|e| e.powf(exponent)).collect(),
             ..*self
         }
+    }
+    /// square: same as power but with fixed exponent = 2
+    pub fn square(&self) -> Self {
+        self.power(2.)
     }
     /// sqrt: of the matrix
     pub fn sqrt(&self) -> Self {
@@ -153,28 +209,129 @@ impl Matrix {
 
     //// Trigonometric functions
     //// - https://numpy.org/doc/stable/reference/routines.math.html#trigonometric-functions
-    // sin
-    // cos
-    // tan
-    // arcsin
-    // arccos
-    // arctan
-    // hypot
+    /// sin
+    pub fn sin(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.sin()).collect(),
+            ..*self
+        }
+    }
+    /// cos
+    pub fn cos(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.cos()).collect(),
+            ..*self
+        }
+    }
+    /// tan
+    pub fn tan(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.tan()).collect(),
+            ..*self
+        }
+    }
+    /// arcsin
+    pub fn arcsin(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.asin()).collect(),
+            ..*self
+        }
+    }
+    /// arccos
+    pub fn arccos(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.acos()).collect(),
+            ..*self
+        }
+    }
+    /// arctan
+    pub fn arctan(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.atan()).collect(),
+            ..*self
+        }
+    }
+    /// hypot
+    pub fn hypot(&self, matrix_2: &Self) -> Self {
+        Matrix {
+            mem: self
+                .mem
+                .iter()
+                .zip(matrix_2.mem.iter())
+                .map(|(&a, &b)| a.hypot(b))
+                .collect(),
+            ..*self
+        }
+    }
     // arctan2
-    // degrees
-    // radians
+
+    /// degrees: Convert angles from radians to degrees.
+    ///
+    /// [&self:] matrix of radians
+    pub fn degrees(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.to_degrees()).collect(),
+            ..*self
+        }
+    }
+    /// radians: Convert angles from degress to radians.
+    ///
+    /// [&self:] matrix of degrees
+    pub fn radians(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.to_radians()).collect(),
+            ..*self
+        }
+    }
     // unwrap
     // deg2rad
     // rad2deg
 
     //// Hyperbolic functions
     //// https://numpy.org/doc/stable/reference/routines.math.html#hyperbolic-functions
-    // sinh
-    // cosh
-    // tanh
-    // arcsinh
-    // arccosh
-    // arctanh
+
+    /// sinh
+    pub fn sinh(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.sinh()).collect(),
+            ..*self
+        }
+    }
+    /// cosh
+    pub fn cosh(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.cosh()).collect(),
+            ..*self
+        }
+    }
+    /// tanh
+    pub fn tanh(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.tanh()).collect(),
+            ..*self
+        }
+    }
+    /// arcsinh
+    pub fn arcsinh(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.asinh()).collect(),
+            ..*self
+        }
+    }
+    /// arccosh
+    pub fn arccosh(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.acosh()).collect(),
+            ..*self
+        }
+    }
+    /// arctanh
+    pub fn arctanh(&self) -> Self {
+        Matrix {
+            mem: self.mem.iter().map(|e| e.atanh()).collect(),
+            ..*self
+        }
+    }
 }
 
 #[cfg(test)]
